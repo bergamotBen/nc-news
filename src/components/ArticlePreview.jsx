af3065e99425c8e0892be0f4cf41ca7f96a035df
bergamotBen <bergamotBen@icloud.com>
2023-02-06 15:36:23 +0000

    view all articles complete

--- src/components/ArticlePreview.jsx
+++ src/components/ArticlePreview.jsx
@@ -3,11 +3,14 @@ const ArticlePreview = (article) => {
   return (
     <Link to={`/articles/${article.article.article_id}`}>
       <div id="articlePreview">
-        <img src={article.article.article_img_url} />
+        <img
+          src={article.article.article_img_url}
+          alt={article.article.title}
+        />
         <h3 id="articleTitle">{article.article.title}</h3>
         <h4 id="articleAuthor">{article.article.author}</h4>
         <div id="details">
-          votes: {article.article.votes} comments:{" "}
+          votes: {article.article.votes} comments:
           {article.article.comment_count}
         </div>
       </div>
