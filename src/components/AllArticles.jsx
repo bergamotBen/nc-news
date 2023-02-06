37302cb4bba29ed4cb2cc92e404513185ef19b85
bergamotBen <bergamotBen@icloud.com>
2023-02-06 16:03:07 +0000

    loading functionality added to article previews

--- src/components/AllArticles.jsx
+++ src/components/AllArticles.jsx
@@ -3,12 +3,17 @@ import { useState, useEffect } from "react";
 import { getAllArticles } from "../utils/apiRequests";
 
 const AllArticles = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [articlePreviews, setArticlePreviews] = useState([]);
   useEffect(() => {
     getAllArticles().then((articles) => {
       setArticlePreviews(articles.articles);
+      setIsLoading(false);
     });
   }, []);
+  if (isLoading) {
+    return <p>loading</p>;
+  }
   return (
     <section id="allArticles">
       <h3>all articles</h3>
