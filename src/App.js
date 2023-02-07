eb3cffa9a0906b525606a8af94d39ff03983abb4
bergamotBen <bergamotBen@icloud.com>
2023-02-07 13:55:17 +0000

    user context created

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import Logo from "./components/Logo";
 import Nav from "./components/Nav";
 import ReadArticle from "./components/ReadArticle";
 import { Routes, Route } from "react-router-dom";
+// import { useContext } from "react";
+// import { UserContext } from "./contexts/UserContext";
 
 function App() {
+  // const userValue = useContext(UserContext);
   return (
     <div className="App">
       <Logo />
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import { UserProvider } from "./contexts/UserContext";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
